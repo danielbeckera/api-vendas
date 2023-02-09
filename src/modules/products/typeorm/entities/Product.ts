21a5c94775ff8eee9406f2f9d55913ca706bc824
Daniel Becker <danielbeckeralves@gmail.com>
2023-02-08 18:26:30 -0800

    created product entity

---
+++ src/modules/products/typeorm/entities/Product.ts
@@ -0,0 +1,30 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('products')
+class Product {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column('decimal')
+  price: number;
+
+  @Column('int')
+  quantity: number;
+
+  @CreateDateColumn('timestamp with time zone')
+  created_at: Date;
+
+  @UpdateDateColumn('timestamp with time zone')
+  updated_at: Date;
+}
+
+export default Product;
