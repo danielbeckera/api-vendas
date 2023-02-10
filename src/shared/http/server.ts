44d9b1ccb547a2c09efcaf5301cd881ee4e2c5f4
Daniel Becker <danielbeckeralves@gmail.com>
2023-02-09 20:29:33 -0800

    added import to express async errors to handle err

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
 import express, { NextFunction, Request, response, Response } from 'express';
+import 'express-async-errors';
 import cors from 'cors';
 import routes from './routes';
 import AppError from '@shared/errors/AppError';
