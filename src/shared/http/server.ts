159ea9cc071bd2c4e1ba026166e7a0ee738c88e8
Daniel Becker <danielbeckeralves@gmail.com>
2023-02-09 20:52:29 -0800

    added middleware to capture errors from celebrate

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 import express, { NextFunction, Request, response, Response } from 'express';
 import 'express-async-errors';
+import { errors } from 'celebrate';
 import cors from 'cors';
 import routes from './routes';
 import AppError from '@shared/errors/AppError';
@@ -12,6 +13,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+
+app.use(errors());
 
 app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
   if (error instanceof AppError) {
